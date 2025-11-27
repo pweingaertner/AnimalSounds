aebd8e8d8c9ff56cf3a48ea2f36adfc9328b063e
Paul Weingärtner <paul-weingaertner@gmx.net>
2025-11-27 11:22:14 +0100

    Error message stuff

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -15,12 +15,49 @@
 #' @examples
 #' animal_sounds("dog", "woof")
 
+#animal_sounds <- function(animal, sound) {
+#  stopifnot(is.character(animal) & length(animal) == 1)
+#  stopifnot(is.character(sound) & length(sound) == 1)
+#
+#  paste0("The ", animal, " says ", sound, "!")
+#}
+
+
+
+#animal_sounds <- function(animal, sound) {
+
+#  if(!rlang::is_character(animal, n=1)){
+#    cli::cli_abort(c("{.var animal} must be a single string",
+#                     "i"= "It was {.type {animal}} of length {length(animal)} instead."))
+#  }
+
+#  if(!rlang::is_character(sound, n=1)){
+#    cli::cli_abort("`sound` must be a single string")
+#  }
+#
+#  paste0("The ", animal, " says ", sound, "!")
+#}
+
+
+
+
+check_arg <- function(arg, n = 1){
+
+  if(!rlang::is_character(arg, n=n)){
+    cli::cli_abort(c("{.var {rlang::caller_arg(arg)}} must be a character vector of length {n}.",
+                     "i"= "It was {.type {arg}} of length {length(arg)} instead."),
+                   call = rlang::caller_env())
+  }
+}
+
+
+
 animal_sounds <- function(animal, sound) {
-  stopifnot(is.character(animal) & length(animal) == 1)
-  stopifnot(is.character(sound) & length(sound) == 1)
+
+  check_arg(animal)
+  check_arg(sound)
 
   paste0("The ", animal, " says ", sound, "!")
 }
-
 
 
